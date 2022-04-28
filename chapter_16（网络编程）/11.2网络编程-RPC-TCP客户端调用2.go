295e5f8bafa08c622121ac95fac52a2b132960c1
LZK <LZK916990143@outlook.com>
2022-04-28 14:45:47 +0800

    second commit

--- chapter_16（网络编程）/11.2网络编程-RPC-TCP客户端调用2.go
+++ chapter_16（网络编程）/11.2网络编程-RPC-TCP客户端调用2.go
@@ -7,10 +7,13 @@ import (
 )
 
 func main() {
+	//1.建立连接
 	client, err := rpc.Dial("tcp", "localhost:8082")
 	if err != nil {
 		log.Fatal("dialing:", err)
 	}
+
+	//2.调用
 	var reply string
 	err = client.Call("HelloService.Hello", "world", &reply)
 	if err != nil {
